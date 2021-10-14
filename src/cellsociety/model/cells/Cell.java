207a4f31075f43b803a0461e642a327b73471b46
Marcus Deans <marcus.deans@duke.edu>
2021-10-13 23:08:46 -0400

    Added method to compute neighbors for any given cel

--- src/cellsociety/model/cells/Cell.java
+++ src/cellsociety/model/cells/Cell.java
@@ -2,8 +2,18 @@ package cellsociety.model.cells;
 
 public abstract class Cell {
   int myCellState;
+  int myX;
+  int myY;
 
   public Cell(int cellState) {
     myCellState = cellState;
+  }
+
+  public int getMyY() {
+    return myY;
+  }
+
+  public int getMyX() {
+    return myX;
   }
 }
--- src/cellsociety/model/gamegrids/GameGrid.java
+++ src/cellsociety/model/gamegrids/GameGrid.java
@@ -4,10 +4,15 @@ import cellsociety.model.cells.Cell;
 
 public abstract class GameGrid {
     Cell[][] myGameGrid;
+    int myGameWidth;
+    int myGameHeight;
+    Cell[] checkingCellNeighbours;
     int myNewValue;
 
     public GameGrid(Cell[][] gameGrid){
       myGameGrid = gameGrid;
+      myGameWidth = gameGrid.length;
+      myGameHeight = gameGrid[0].length;
       myNewValue = 0;
     }
 
@@ -18,8 +23,23 @@ public abstract class GameGrid {
 
     }
 
+    //populates Cell[] of the possible neighbours of given cell (max 9)
     protected void computeNeighbours(Cell checkingCell){
-
+      checkingCellNeighbours = new Cell[9];
+      int iterator = 0;
+      for(int x = -1; x < 2; x++){
+        int checkX = checkingCell.getMyX() + x;
+        if(checkX < 0 || checkX > myGameWidth){
+          continue;
+        }
+        for(int y = -1; y<2; y++){
+          int checkY = checkingCell.getMyY() + y;
+          if(checkY < 0 || checkY > myGameHeight){
+            continue;
+          }
+          checkingCellNeighbours[iterator] = myGameGrid[checkX][checkY];
+        }
+      }
     }
 
     private int newValue(){
