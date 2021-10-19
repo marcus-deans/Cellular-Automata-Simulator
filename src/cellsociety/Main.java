0af445bd3cea18f79c056fab7efb25d0da725f4a
Morgan Feist <mjf58@duke.edu>
2021-10-19 11:01:21 -0400

    thinking about main

--- src/cellsociety/Main.java
+++ src/cellsociety/Main.java
@@ -1,21 +1,24 @@
 package cellsociety;
 
 
+import cellsociety.controller.GameController;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * Feel free to completely change this code or delete it entirely. 
  */
-public class Main {
-    /**
-     * A method to test (and a joke :).
-     */
-    public double getVersion () {
-        return 0.001;
-    }
+public class Main extends Application {
 
+    @Override
+    public void start(Stage stage) {
+        GameController controller=new GameController();
+    }
     /**
      * Start of the program.
      */
-    public static void main (String[] args) {
-        System.out.println("Hello world");
-    }
+//    public static void main (String[] args) {
+//        //make controller? make view?
+//        GameController controller=new GameController();
+//    }
 }
