3ee0945e8b9fa0c5b36f4eca992bea07c9445787
Marcus Deans <marcus.deans@duke.edu>
2021-10-23 17:50:22 -0400

    Added required classes and rules for Percolation to be executed

---
+++ src/cellsociety/model/gamegrids/PercGrid.java
@@ -0,0 +1,65 @@
+package cellsociety.model.gamegrids;
+
+import cellsociety.model.cells.Cell;
+import cellsociety.model.cells.FireCell.FIRE_STATES;
+import cellsociety.model.cells.PercCell.PERC_STATES;
+import java.util.Random;
+
+public class PercGrid extends GameGrid{
+  public PercGrid(Cell[][] gameGrid){
+    super(gameGrid);
+  }
+
+  @Override
+  public void runGame() {
+    computeNeighborsAndRules();
+  }
+
+  private PERC_STATES determineCellState(int newValue){
+    switch(newValue){
+      case 0 -> {
+        return PERC_STATES.EMPTY;
+      }
+      case 1 -> {
+        return PERC_STATES.BLOCKED;
+      }
+      case 2 -> {
+        return PERC_STATES.PERCOLATED;
+      }
+    }
+    return PERC_STATES.ERROR;
+  }
+
+  //apply the rules of Percolation  -> go through neighbours and check which conditions satisfied
+  //also check cases for self
+  //store new value for given cell in futureGrid
+  protected void applyGameRules(Cell computingCell, int col, int row){
+    int currentCellState = computingCell.getMyCellState();
+    int newValue = currentCellState;
+
+    switch(determineCellState(currentCellState)){
+      case EMPTY -> {
+        for(Cell neighbouringCell : checkingCellNeighbours) {
+          if (neighbouringCell != null) {
+            if (neighbouringCell.getMyCellState() == PERC_STATES.PERCOLATED.getValue()) {
+              newValue = PERC_STATES.PERCOLATED.getValue();
+              break;
+            }
+          }
+        }
+      }
+      case BLOCKED -> {
+        newValue = PERC_STATES.BLOCKED.getValue();
+      }
+      case PERCOLATED -> {
+        newValue = PERC_STATES.PERCOLATED.getValue();
+      }
+      default -> {
+        newValue = PERC_STATES.ERROR.getValue();
+      }
+    }
+
+    futureGrid[row][col].setMyCellState(newValue);
+  }
+
+}
